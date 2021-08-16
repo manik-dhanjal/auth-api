4ddb4f8a67d26f799147c0d09d4236a4be1ded7e
Team2626Creative <manikdhanjal217@gmail.com>
2021-08-17 03:27:04 +0530

    404 handled

--- index.js
+++ index.js
@@ -12,11 +12,28 @@ app.use(express.json());
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
+
 app.get('/home',authentication,(req,res)=>{
     res.send(`welcome home ${req.user.name} _/\\_`)
 })
+
 app.use('/',[signUp,login])
 
+app.use(function(req, res, next) {
+    res.status(404);
+  
+    if (req.accepts('html')) {
+      res.render('404', { url: req.url });
+      return;
+    }
+  
+    if (req.accepts('json')) {
+      res.json({ error: 'Not found' });
+      return;
+    }
+  
+    res.type('txt').send('Not found');
+  });
 const PORT = process.env.PORT||8000;
 
 app.listen(PORT,()=>{
